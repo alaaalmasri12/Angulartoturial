e650364ec8e26dd7ef55f621d61a451973e51457
alaaalmasri12 <alaa_almasri1997@yahoo.com>
2023-05-16 07:15:53 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
-
+const routes:Routes=[
+  {path:'',pathMatch:'full', redirectTo:'home'},
+  {path:'home',component:HomeComponent,title:'home',
+},
+  {path:'about',component:AboutComponent,title:'about'},
+  {path:'test',component:ContactComponent,title:"contact"}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
